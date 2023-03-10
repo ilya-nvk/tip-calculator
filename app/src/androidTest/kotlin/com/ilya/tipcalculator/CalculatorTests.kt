65c1579aa9ffb01f430e95c7cfcb6d7bb9cb7a27
Ilya <iinovik@edu.hse.ru>
2023-03-10 12:40:25 +0300

    Pathway 2 completed

--- app/src/androidTest/kotlin/com/ilya/tipcalculator/CalculatorTests.kt
+++ app/src/androidTest/kotlin/com/ilya/tipcalculator/CalculatorTests.kt
@@ -1,7 +1,45 @@
 package com.ilya.tipcalculator
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.hamcrest.Matchers.containsString
 
 @RunWith(AndroidJUnit4::class)
 class CalculatorTests {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.option_twenty_percent)).perform(click())
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$10.00"))))
+    }
+
+    @Test
+    fun calculate_empty_string() {
+        calculate_20_percent_tip()
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText(""))
+        onView(withId(R.id.calculate_button)).perform(click())
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString(""))))
+    }
 }
